565cbb5bfdd2ad582ea0d4d3eaba687fcd568c83
Fernando Henrique Dalua <dalua_fernando@hotmail.com>
2019-08-30 15:22:15 -0300

    Ajustes para receber parametros na execução do desafio 2

--- Desafio2/compara_arquivos.py
+++ Desafio2/compara_arquivos.py
@@ -1,19 +1,33 @@
-import threading
+import threading, sys
 
 class Arquivo(threading.Thread):
-    def __init__(self):
+    def __init__(self, param):
         threading.Thread.__init__(self)
+        self.param = param
 
     def run(self):
-        print('oi')
+        with open(self.param[1], 'r') as t1, open(self.param[2], 'r') as t2:
+            fileOne = t1.readlines()
+            fileTwo = t2.readlines()
 
+        with open('saida.csv', 'w') as outFile:
+            for line in fileTwo:
+                if line in fileOne:
+                    outFile.write(line)
 
-
-
+        t1.close()
+        t2.close()
+        outFile.close()
+        print('Arquivos comparados com sucesso')
 
 
 def main():
-    arquivo = Arquivo()
+    param = []    
+    for parametro in sys.argv:
+        print(parametro)        
+        param.append(parametro)                
+        
+    arquivo = Arquivo(param)
     arquivo.run()
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ### • Desafio 1 - Web Crawler de busca aos produtos do setor de petshop no site do extra.com.br
 ### • Desafio 2 - Comparar 2 arquivos 
 
+####	**Para executar**
+		compara_arquivos.py _arquivo1.csv_ _arquivo2.csv_ 
+
